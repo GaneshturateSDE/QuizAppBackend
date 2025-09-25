6b228042343cd342ea181fc56b526ef39e61078d
GaneshturateSDE <ganeshturate1@gmail.com>
2025-09-26 01:12:58 +0530

    Question schema validation

---
+++ src/Validations/question.validation.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+import { CATGEORY } from "../constants/constants.js";
+
+
+const questionValidationSchema = Joi.object({
+  title: Joi.string().min(3).max(255).required(),
+  options: Joi.array().items(Joi.string().min(1).max(255)).min(2).required(),
+  answer: Joi.string().min(1).max(255).required(),
+  category: Joi.string()
+    .valid(...CATGEORY)
+    .required(),
+});
+
+export { questionValidationSchema };
