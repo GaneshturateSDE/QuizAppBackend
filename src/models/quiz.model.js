e6adb2569aa388c8071b0cf846b2244a2c2df99d
GaneshturateSDE <ganeshturate1@gmail.com>
2025-09-26 04:04:48 +0530

    quiz model schema

---
+++ src/models/quiz.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+
+const quizSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  questions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question", 
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const quizModel= mongoose.model("Quiz", quizSchema);
+
+export default quizModel;
