0ac2e4f36e3ff6aafc43603184931279c07da9d3
GaneshturateSDE <ganeshturate1@gmail.com>
2025-09-26 04:05:15 +0530

    updated  question schema

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -2,16 +2,17 @@
 
 import mongoose from "mongoose";
 import { CATGEORY } from "../constants/constants.js";
+
+const optionSchema = new mongoose.Schema({
+  option: { type: String, required: true },                   
+},{ _id: true });
 const questionSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
   },
   options: [
-    {
-      type: String,
-      required: true,
-    },
+          optionSchema,
   ],
   answer: {
     type: String,
@@ -23,6 +24,8 @@ const questionSchema = new mongoose.Schema({
     enum:CATGEORY
   },
 });
+
+
 
 const questionModel= mongoose.model("Question", questionSchema);
 export default questionModel;
